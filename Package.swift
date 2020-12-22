824492c23a514baf44a00af17af4c2f1f0f582ba
Krunal Chaudhari <krunal.c@avdevs.com>
2020-12-22 11:21:03 +0530

    change version 1.1.3

--- Package.swift
+++ Package.swift
@@ -28,6 +28,5 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "mvCardPackageTests",
-            dependencies: ["mvCardPackage"]),
-    ]
+            dependencies: ["mvCardPackage"])]
 )
