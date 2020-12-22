e66931e51cad92690fc14b817cbca9fb9733437d
Krunal Chaudhari <krunal.c@avdevs.com>
2020-12-22 11:34:16 +0530

    new version 2.0.0

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "mvCardPackage",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "mvCardPackageTests",
             dependencies: ["mvCardPackage"]),
